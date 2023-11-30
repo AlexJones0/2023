204b5ff3639a01adc2017575f77c054d5fcba16d
xeovz <65985521+xeovz@users.noreply.github.com>
2023-11-30 18:39:19 +0000

    Add day 1 python boilerplate

---
+++ Day 01/Python/sol.py
@@ -0,0 +1,13 @@
+"""
+FILE: Day 01/sol.py
+Author: Alex Jones
+Desc: Solution to day 1 problems (1 & 2) for Advent of Code 2023, solved in Python 3.
+"""
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 01/data.txt", "r").read().split("\n")]
+
+
+
+print("Problem 1:", NOT_IMPLEMENTED)
+
+print("Problem 2:", NOT_IMPLEMENTED)
