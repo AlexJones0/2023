236afadffa8f8f436fa5a72e83b84bbaa2852bbd
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-01 12:03:49 +0000

    Slightly cleaned day 1 python solutions

--- Day 01/Python/sol.py
+++ Day 01/Python/sol.py
@@ -6,22 +6,25 @@ Desc: Solution to day 1 problems (1 & 2) for Advent of Code 2023, solved in Pyth
 NOT_IMPLEMENTED = "Not Yet Implemented"
 data = [x for x in open("Day 01/data.txt", "r").read().split("\n")]
 
-
+# Find all digits, take the first and last, and work out the calibration values
 digits = [[c for c in line if c.isdigit()] for line in data]
-print("Problem 1:", sum([int(digitList[0] + digitList[-1]) for digitList in digits if len(digitList) > 0]))
+print("Problem 1:", sum([int(digitList[0] + digitList[-1]) for digitList in digits]))
 
 
+digitIndexes = [{i: int(c) for i, c in enumerate(line) if c.isdigit()} for line in data]
+
+# In addition to the above, we now store the indexes of our digits to allow easy comparison
+# and search for occurrences of all words in each line, marking these accordingly
 words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-digitIndexes = [{i: c for i, c in enumerate(line) if c.isdigit()} for line in data]
 for i, line in enumerate(data):
     line = line.lower()
-    for n, word in enumerate(words):
+    for valuePos, word in enumerate(words):
         digitPos = 0
         while digitPos != -1:
             digitPos = line.find(word, digitPos)
             if digitPos != -1:
-                digitIndexes[i][digitPos] = str(n + 1)
+                digitIndexes[i][digitPos] = valuePos + 1
                 digitPos += 1
-firstDigit = [lineDict[min(lineDict.keys())] for lineDict in digitIndexes if len(lineDict) > 0]
-lastDigit = [lineDict[max(lineDict.keys())] for lineDict in digitIndexes if len(lineDict) > 0]
-print("Problem 2:", sum([int(f + l) for f, l in zip(firstDigit, lastDigit)]))
+# Again we combine the first and last digits to get calibration values, this time searching by min/max index
+calibrationValues = [lineDict[min(lineDict.keys())] * 10 + lineDict[max(lineDict.keys())] for lineDict in digitIndexes]
+print("Problem 2:", sum(calibrationValues))
