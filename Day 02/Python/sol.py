31737578693b9e2ffbc50d580e1b818c676137af
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-02 15:08:58 +0000

    Full cleanup of python day 2 solutions

--- Day 02/Python/sol.py
+++ Day 02/Python/sol.py
@@ -6,49 +6,39 @@ Desc: Solution to day 2 problems (3 & 4) for Advent of Code 2023, solved in Pyth
 NOT_IMPLEMENTED = "Not Yet Implemented"
 data = [x.strip() for x in open("Day 02/data.txt", "r").read().split("\n") if len(x.strip()) > 0]
 
+
+# For part 1 we assume that cubes of the same c olour do not appear in the same reveal (in the same game); this assumption holds
+# for the input data, but if it was not true, we would need to track local amounts per reveal and update the max based on this.
+
+# For part 2 we assume that only red, green and blue cubes are included in each game; which holds for the given data set.
+
+
 from functools import reduce
 from collections import defaultdict
-
-
-targetCubeAmounts = {
-    "blue": 14,
-    "red": 12,
-    "green": 13
-}
 
 
 def getGameId(line: str) -> int:
     return int(line.split(":")[0].split(" ")[-1])
 
 
-def getGameData(line: str) -> dict[str,int]:
+def getGameData(line: str) -> defaultdict[str,int]:
     gameData = defaultdict(int)
     for reveal in line.split(":")[1].split(";"):
-        # Assume cubes of the same colour do not appear in the same reveal; this assumption holds for the input data
-        # but if not true, we would need to track local amounts per reveal and update the max based on this.
         for cubeStr in reveal.split(", "):
-            cubeStr = cubeStr.strip()
-            if len(cubeStr) == 0:
-                continue
-            cubeInfo = cubeStr.split(" ")
-            amount = int(cubeInfo[0])
-            colour = cubeInfo[1].lower()
-            gameData[colour] = max(gameData[colour], amount)
+            (amount, colour) = cubeStr.strip().split(" ")
+            gameData[colour] = max(gameData[colour], int(amount))
     return gameData
 
 
-def isGamePossible(gameData: dict[str,int], targets: dict[str,int]) -> bool:
-    for colour, amount in gameData.items():
-        if colour not in targets or targets[colour] < amount:
-            return False
-    return True
+def isGamePossible(gameData: defaultdict[str,int], targets: dict[str,int]) -> bool:
+    return all(gameData[colour] <= amount for colour, amount in targets.items())
 
 
 def getGamePower(line: str) -> int:
     return reduce(lambda x,y: (x*y), getGameData(line).values(), 1)
 
 
-print("Problem 3:", sum([getGameId(line) for line in data if isGamePossible(getGameData(line), targetCubeAmounts)]))
+targetCubeAmounts = {"blue": 14, "red": 12, "green": 13}
+print("Problem 3:", sum(getGameId(line) for line in data if isGamePossible(getGameData(line), targetCubeAmounts)))
 
-# For this we make an assumption that only red green and blue cubes are included in the set; this holds for the data
-print("Problem 4:", sum([getGamePower(line) for line in data]))
+print("Problem 4:", sum(getGamePower(line) for line in data))
