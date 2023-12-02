f253a925bb23b9cb9b379fb94ba2204335263a01
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-02 14:59:42 +0000

    Modularise game power for readability

--- Day 02/Python/sol.py
+++ Day 02/Python/sol.py
@@ -44,7 +44,11 @@ def isGamePossible(gameData: dict[str,int], targets: dict[str,int]) -> bool:
     return True
 
 
+def getGamePower(line: str) -> int:
+    return reduce(lambda x,y: (x*y), getGameData(line).values(), 1)
+
+
 print("Problem 3:", sum([getGameId(line) for line in data if isGamePossible(getGameData(line), targetCubeAmounts)]))
 
 # For this we make an assumption that only red green and blue cubes are included in the set; this holds for the data
-print("Problem 4:", sum([reduce(lambda x,y: (x*y), getGameData(line).values(), 1) for line in data]))
+print("Problem 4:", sum([getGamePower(line) for line in data]))
