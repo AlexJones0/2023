016594d1edc4cb0292a957760cafa3ed7e547a70
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-04 05:28:52 +0000

    Clean up and comment day 4 python sols

--- Day 04/Python/sol.py
+++ Day 04/Python/sol.py
@@ -2,38 +2,33 @@
 FILE: Day 04/sol.py
 Author: Alex Jones
 Desc: Solution to day 4 problems (7 & 8) for Advent of Code 2023, solved in Python 3.
+Rank: 252/110
 """
 NOT_IMPLEMENTED = "Not Yet Implemented"
 data = [x.strip() for x in open("Day 04/data.txt", "r").read().split("\n") if len(x.strip()) > 0]
 
-total = 0
-for line in data:
-    line = line.split(":")[1].split("|")
-    winning = [int(n.strip()) for n in line[0].strip().split(" ") if len(n.strip()) > 0]
-    yours = [int(n.strip()) for n in line[1].strip().split(" ") if len(n.strip()) > 0]
-    intersect = set(winning).intersection(set(yours))
-    if len(intersect) > 0:
-        total += 2 ** (len(intersect) - 1)
 
-print("Problem 7:", total)
+# Problem 7 approach: parse line into a set of winning numbers and our numbers, perform set intersection,
+# and get scores by summing 2 to the power of the size of the intersection minus 1 (unless it is 0).
 
-total = 0
-amount = []
-for line in data:
-    number_of_cards = 1
-    if len(amount) > 0:
-        number_of_cards += amount[0]
-        amount = amount[1:]
-    total += number_of_cards
-    line = line.split(":")[1].split("|")
-    winning = [int(n.strip()) for n in line[0].strip().split(" ") if len(n.strip()) > 0]
-    yours = [int(n.strip()) for n in line[1].strip().split(" ") if len(n.strip()) > 0]
-    intersect = set(winning).intersection(set(yours))
-    for i in range(len(intersect)):
-        if i < len(amount):
-            amount[i] += number_of_cards
-        else:
-            amount.append(number_of_cards)
+def parseNumbers(numberStr: str) -> set[str]:
+    return set([n.strip() for n in numberStr.strip().split(" ") if n != ""])
+
+data = [line.split(":")[1].split("|") for line in data]
+matches = [len(parseNumbers(line[0]).intersection(parseNumbers(line[1]))) for line in data]
+
+print("Problem 7:", sum(2 ** (n-1) if n > 0 else 0 for n in matches))
 
 
-print("Problem 8:", total)
+# Problem 8 approach: using the prev calculated num of matches, construct a list of the number of each
+# card by traversing the list in order, and sum the resulting list at the end. We could optimise to O(1) 
+# space using the fact that there is only ever <= 10 winning numbers, but this is not a problem here and 
+# we are trying to make a general solution, so we don't bother.
+
+numOfCards = [1 for _ in range(len(data))]
+for i, card in enumerate(matches):
+    maxFollowIndex = min(len(matches),i+1+matches[i])
+    for j in range(i+1, maxFollowIndex):
+        numOfCards[j] += numOfCards[i]
+
+print("Problem 8:", sum(numOfCards))
