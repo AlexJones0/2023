96ab9120d5b559f948f09a7e5336a24648f785d3
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-06 05:27:36 +0000

    Clean and comment day 6 python solutions

--- Day 06/Python/sol.py
+++ Day 06/Python/sol.py
@@ -1,41 +1,28 @@
 """
-FILE: Day 05/sol.py
+FILE: Day 06/sol.py
 Author: Alex Jones
-Desc: Solution to day 5 problems (9 & 10) for Advent of Code 2023, solved in Python 3.
-Rank: 463/77
-Comments: I ran into so many issues on part 1 because of a bug in my boilerplate that I stopped trying for speed (as is evident in my part 1 rank),
-  but unexpectedly after realising a brute force wouldn't work for part 2 I managed to code up the relevant interval tracking solution very quickly
-  with no bugs and put myself on the leaderboard for the first time. I'm guessing most people found either the algorithm or the small details
-  of implementing part 2 difficult.
+Desc: Solution to day 6 problems (11 & 12) for Advent of Code 2023, solved in Python 3.
 """
 NOT_IMPLEMENTED = "Not Yet Implemented"
 data = open("Day 06/data.txt", "r").read().split("\n")
 
+# Idea: the distance travelled is (race time - time waited) * speed
+# But because we gain 1 speed per second waited, this makes our distance (time - speed) * speed = time * speed - speed^2
+# This is a second order polynomial with a negative x^2 coeffcient - thus we can find all values where distance > record
+# by examining the roots of the polynomial: -speed^2 + time * speed - record.
+# We simply solve using the quadratic formula (simplifying, as a=-1 always), and find the number of discrete speeds/times
+# in that closed range by calculating floor(upper root) - ceil(lower root) + 1.  
+
 from math import prod, sqrt, floor, ceil
 
+def getWays(time, record):
+    det = sqrt(time * time - 4 * record)
+    return floor((time + det) / 2) - ceil((time - det) / 2) + 1
+
 times = [int(n) for n in data[0].split(":")[1].split()]
-distances = [int(n) for n in data[1].split(":")[1].split()]
-
-raceWays = []
-for time, record in zip(times, distances):
-    ways = 0
-    for speed in range(1, time):
-        distance = (time - speed) * speed
-        if distance > record:
-            ways += 1
-    raceWays.append(ways)
-
-print("Problem 11:", prod(raceWays))
+records = [int(n) for n in data[1].split(":")[1].split()]
+print("Problem 11:", prod([getWays(time, record) for time, record in zip(times, records)]))
 
 time = int(data[0].split(":")[1].replace(" ",""))
 record = int(data[1].split(":")[1].replace(" ",""))
-
-a = -1
-b = time
-c = -record
-determinant = sqrt(b * b - 4 * a * c)
-roots = [(-b - determinant) / (2 * a), (-b + determinant) / (2 * a)]
-maxDist = floor(min(time, max(roots)))
-minDist = ceil(max(min(roots), 1))
-
-print("Problem 12:", maxDist - minDist + 1)
+print("Problem 12:", getWays(time, record))
