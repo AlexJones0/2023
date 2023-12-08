330a3b5598ffe81c98d6f5d4babf6dab06e94db1
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-08 06:16:56 +0000

    Simplify day 2 python logic

--- Day 02/Python/sol.py
+++ Day 02/Python/sol.py
@@ -4,41 +4,26 @@ Author: Alex Jones
 Desc: Solution to day 2 problems (3 & 4) for Advent of Code 2023, solved in Python 3.
 """
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = [x.strip() for x in open("Day 02/data.txt", "r").read().split("\n") if len(x.strip()) > 0]
-
+data = [x.split(":") for x in open("Day 02/data.txt", "r").read().strip().split("\n")]
 
 # For part 1 we assume that cubes of the same c olour do not appear in the same reveal (in the same game); this assumption holds
 # for the input data, but if it was not true, we would need to track local amounts per reveal and update the max based on this.
 
-# For part 2 we assume that only red, green and blue cubes are included in each game; which holds for the given data set.
-
-
 from math import prod
 from collections import defaultdict
 
-
-def getGameId(line: str) -> int:
-    return int(line.split(":")[0].split(" ")[-1])
-
-
-def getGameData(line: str) -> defaultdict[str,int]:
+def getGame(line: str) -> defaultdict[str,int]:
     gameData = defaultdict(int)
-    for reveal in line.split(":")[1].split(";"):
-        for cubeStr in reveal.split(", "):
-            (amount, colour) = cubeStr.strip().split(" ")
-            gameData[colour] = max(gameData[colour], int(amount))
+    for reveal in line.replace(";",",").split(","):
+        (amount, colour) = reveal.split()
+        gameData[colour] = max(gameData[colour], int(amount))
     return gameData
 
-
-def isGamePossible(gameData: defaultdict[str,int], targets: dict[str,int]) -> bool:
+def isPossible(gameData: defaultdict[str,int], targets: dict[str,int]) -> bool:
     return all(gameData[colour] <= amount for colour, amount in targets.items())
 
+targets = {"blue": 14, "red": 12, "green": 13}
+print("Problem 3:", sum(int(id_.split()[-1]) for id_, line in data if isPossible(getGame(line), targets)))
 
-def getGamePower(line: str) -> int:
-    return prod(getGameData(line).values())
-
-
-targetCubeAmounts = {"blue": 14, "red": 12, "green": 13}
-print("Problem 3:", sum(getGameId(line) for line in data if isGamePossible(getGameData(line), targetCubeAmounts)))
-
-print("Problem 4:", sum(getGamePower(line) for line in data))
+# For part 2 we assume that only red, green and blue cubes are included in each game; which holds for the given data set.
+print("Problem 4:", sum(prod(getGame(line).values()) for _, line in data))
