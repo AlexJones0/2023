4166e98e1bc03c7fd6d0bc7f2edbcb267d6309ce
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-08 06:08:05 +0000

    Minor cleanup

--- Day 01/Python/sol.py
+++ Day 01/Python/sol.py
@@ -4,7 +4,7 @@ Author: Alex Jones
 Desc: Solution to day 1 problems (1 & 2) for Advent of Code 2023, solved in Python 3.
 """
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = [x for x in open("Day 01/data.txt", "r").read().split("\n")]
+data = open("Day 01/data.txt", "r").read().split("\n")
 
 def solve(data: list[str]):
     # Find all digits, take the first and last, and work out the calibration values
