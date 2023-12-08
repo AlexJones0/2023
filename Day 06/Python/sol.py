023735b47cd16b91c2e749ff793ac1290b2ac152
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-08 06:50:34 +0000

    Simplify day 6 python solution logic

--- Day 06/Python/sol.py
+++ Day 06/Python/sol.py
@@ -4,7 +4,7 @@ Author: Alex Jones
 Desc: Solution to day 6 problems (11 & 12) for Advent of Code 2023, solved in Python 3.
 """
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = open("Day 06/data.txt", "r").read().split("\n")
+data = [x.split(":")[1] for x in open("Day 06/data.txt", "r").readlines()]
 
 # Idea: the distance travelled is (race time - time waited) * speed
 # But because we gain 1 speed per second waited, this makes our distance (time - speed) * speed = time * speed - speed^2
@@ -15,14 +15,11 @@ data = open("Day 06/data.txt", "r").read().split("\n")
 
 from math import prod, sqrt, floor, ceil
 
-def getWays(time, record):
-    det = sqrt(time * time - 4 * record)
+def getWays(time: int, record: int):
+    det = sqrt(time ** 2 - 4 * record)
     return floor((time + det) / 2) - ceil((time - det) / 2) + 1
 
-times = [int(n) for n in data[0].split(":")[1].split()]
-records = [int(n) for n in data[1].split(":")[1].split()]
-print("Problem 11:", prod([getWays(time, record) for time, record in zip(times, records)]))
+races = [[int(n) for n in line.split()] for line in data]
+print("Problem 11:", prod([getWays(*race) for race in zip(*races)]))
 
-time = int(data[0].split(":")[1].replace(" ",""))
-record = int(data[1].split(":")[1].replace(" ",""))
-print("Problem 12:", getWays(time, record))
+print("Problem 12:", getWays(*[int(line.replace(" ","")) for line in data]))
