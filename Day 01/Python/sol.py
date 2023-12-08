3f071624462b2fad3121c54b553d3a08ab818c18
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-08 06:07:07 +0000

    Simplify day 1 python solution

--- Day 01/Python/sol.py
+++ Day 01/Python/sol.py
@@ -6,25 +6,15 @@ Desc: Solution to day 1 problems (1 & 2) for Advent of Code 2023, solved in Pyth
 NOT_IMPLEMENTED = "Not Yet Implemented"
 data = [x for x in open("Day 01/data.txt", "r").read().split("\n")]
 
-# Find all digits, take the first and last, and work out the calibration values
-digits = [[c for c in line if c.isdigit()] for line in data]
-print("Problem 1:", sum([int(digitList[0] + digitList[-1]) for digitList in digits]))
+def solve(data: list[str]):
+    # Find all digits, take the first and last, and work out the calibration values
+    digits = [[c for c in line if c.isdigit()] for line in data]
+    return sum([int(digitList[0] + digitList[-1]) for digitList in digits])
 
+print("Problem 1:", solve(data))
 
-digitIndexes = [{i: int(c) for i, c in enumerate(line) if c.isdigit()} for line in data]
-
-# In addition to the above, we now store the indexes of our digits to allow easy comparison
-# and search for occurrences of all words in each line, marking these accordingly
-words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-for i, line in enumerate(data):
-    line = line.lower()
-    for valuePos, word in enumerate(words):
-        digitPos = 0
-        while digitPos != -1:
-            digitPos = line.find(word, digitPos)
-            if digitPos != -1:
-                digitIndexes[i][digitPos] = valuePos + 1
-                digitPos += 1
-# Again we combine the first and last digits to get calibration values, this time searching by min/max index
-calibrationValues = [lineDict[min(lineDict.keys())] * 10 + lineDict[max(lineDict.keys())] for lineDict in digitIndexes]
-print("Problem 2:", sum(calibrationValues))
+# Replace all words with numberic occurrences (like one1one to consider overlap) and repeat
+for i in range(len(data)):
+    for n, word in enumerate(["one","two","three","four","five","six","seven","eight","nine"]):
+        data[i] = data[i].replace(word, f"{word}{n+1}{word}")
+print("Problem 2:", solve(data))
