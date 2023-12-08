02bd9a370384305ddc48bf630c1cf27d50cc372e
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-08 06:34:05 +0000

    Minor logic changes for day 4 python sols

--- Day 04/Python/sol.py
+++ Day 04/Python/sol.py
@@ -5,14 +5,12 @@ Desc: Solution to day 4 problems (7 & 8) for Advent of Code 2023, solved in Pyth
 Rank: 252/110
 """
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = [x.strip() for x in open("Day 04/data.txt", "r").read().split("\n") if len(x.strip()) > 0]
+data = [x.line.split(":")[1].split("|") for x in open("Day 04/data.txt", "r").read().strip().split("\n")]
 
 
 # Problem 7 approach: parse line into a set of winning numbers and our numbers, perform set intersection,
 # and get scores by summing 2 to the power of the size of the intersection minus 1 (unless it is 0).
-
-data = [line.split(":")[1].split("|") for line in data]
-matches = [len(set(win.split()).intersection(set(ours.split()))) for (win, ours) in data]
+matches = [len(set(win.split()) & set(ours.split())) for (win, ours) in data]
 print("Problem 7:", sum(2**(n-1) if n else 0 for n in matches))
 
 
@@ -20,7 +18,6 @@ print("Problem 7:", sum(2**(n-1) if n else 0 for n in matches))
 # card by traversing the list in order, and sum the resulting list at the end. We could optimise to O(1) 
 # space using the fact that there is only ever <= 10 winning numbers, but this is not general.
 # This is just an O(n) 1D dynamic programming approach.
-
 numOfCards = [1] * len(data)
 for i in range(len(matches)):
     for j in range(i+1, min(len(matches), i+1+matches[i])):
