51e2a30184983eb19b952adf3fc29f9e534633a7
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-13 07:06:21 +0000

    Switch manual memoization to functools.cache

--- Day 12/Python/sol.py
+++ Day 12/Python/sol.py
@@ -6,32 +6,26 @@ Desc: Solution to day 12 problems (23 & 24) for Advent of Code 2023, solved in P
 NOT_IMPLEMENTED = "Not Yet Implemented"
 data = [line.split() for line in open("Day 12/data.txt", "r").read().strip().split("\n")]
 
-# Top-down dynamic programming approach
-memo = {}
+from functools import cache
 
+@cache
 def findNumArrangements(row: str, damages: tuple[int], mustBeDamaged: bool = False) -> int:
-    key = (row, damages, mustBeDamaged)
-    if key in memo:
-        return memo[key]
     cannotBeDamaged = damages and damages[-1] == 0
     if cannotBeDamaged:
         damages = damages[:-1]
         mustBeDamaged = False
     cannotBeDamaged = cannotBeDamaged or len(damages) == 0
     if len(row) == 0:
-        memo[key] = len(damages) == 0
+        return len(damages) == 0
     elif row.endswith("#") or row.endswith("?") and mustBeDamaged:
         if cannotBeDamaged:
-            memo[key] = 0
-        else:
-            newDamages = tuple(d if (i+1) != len(damages) else (d-1) for i, d in enumerate(damages))
-            memo[key] = findNumArrangements(row[:-1], newDamages, True)
-    elif row.endswith(".") or cannotBeDamaged:
-        memo[key] = 0 if mustBeDamaged else findNumArrangements(row[:-1], damages)
-    else:
+            return 0
         newDamages = tuple(d if (i+1) != len(damages) else (d-1) for i, d in enumerate(damages))
-        memo[key] = findNumArrangements(row[:-1], damages) + findNumArrangements(row[:-1], newDamages, True)
-    return memo[key]
+        return findNumArrangements(row[:-1], newDamages, True)
+    elif row.endswith(".") or cannotBeDamaged:
+        return 0 if mustBeDamaged else findNumArrangements(row[:-1], damages)
+    newDamages = tuple(d if (i+1) != len(damages) else (d-1) for i, d in enumerate(damages))
+    return findNumArrangements(row[:-1], damages) + findNumArrangements(row[:-1], newDamages, True)
 
 def unfold(row: str, damages: tuple[int], n: int) -> tuple[str,tuple[int]]:
     return ("?".join([row] * n), tuple(sum([list(damages)] * n, [])))
