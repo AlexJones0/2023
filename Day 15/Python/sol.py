38069b788261914d3f84b25241c614c0f131bbad
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-15 06:04:32 +0000

    Make python sols slightly simpler

--- Day 15/Python/sol.py
+++ Day 15/Python/sol.py
@@ -11,14 +11,11 @@ from functools import reduce
 hash_ = lambda string: reduce(lambda val, char: (val + ord(char)) * 17 % 256, string, 0)
 print("Problem 29:", sum(hash_(s) for s in data))
 
-map_ = [[] for _ in range(256)]
+map_ = [{} for _ in range(256)]  # As of 3.6, Python dicts maintain insertion ordering by default! 
 for s in data:
-    isAdd, (label, focus) = "=" in s, s.replace("-","=").split("=")
-    box = hash_(label)
-    if isAdd and any(lbl == label for (lbl, _) in map_[box]):
-        map_[box] = [v if v[0] != label else (v[0], int(focus)) for v in map_[box]]
-    elif isAdd:
-        map_[box].append((label, int(focus)))
+    (label, focus) = s.replace("-","=").split("=")
+    if "=" in s:
+        map_[hash_(label)][label] = int(focus)
     else:
-        map_[box] = [v for v in map_[box] if v[0] != label]
-print("Problem 30:", sum(sum((box+1) * slot * focus for slot, (_, focus) in enumerate(xs,start=1)) for box, xs in enumerate(map_)))
+        map_[hash_(label)].pop(label, None)
+print("Problem 30:", sum(box*slot*focus for box, xs in enumerate(map_,1) for slot, focus in enumerate(xs.values(),1)))
