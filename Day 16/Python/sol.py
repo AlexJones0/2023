2b074929a00310ed4056469e9cb4bc39a91c3af6
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-16 14:33:13 +0000

    Replace tuple[int.int] with complex for slightly nicer math

--- Day 16/Python/sol.py
+++ Day 16/Python/sol.py
@@ -6,28 +6,28 @@ Desc: Solution to day 16 problems (31 & 32) for Advent of Code 2023, solved in P
 NOT_IMPLEMENTED = "Not Yet Implemented"
 data = open("Day 16/data.txt", "r").read().strip().splitlines()
 
-def simulateEnergized(startPos: tuple[int,int], startDirection: tuple[int,int]) -> int:
+def simulateEnergized(startPos: complex, startDirection: complex) -> int:
     beams = [(startPos, startDirection)]
     seen = set()
     while beams:
         beamPos, beamDirection = beams.pop()
         if (beamPos, beamDirection) in seen:
             continue
-        nextPos = (beamPos[0] + beamDirection[0], beamPos[1] + beamDirection[1])
         seen.add((beamPos, beamDirection))
-        if not(0 <= nextPos[0] < len(data) and 0 <= nextPos[1] < len(data[0])):
+        beamPos += beamDirection
+        if not(0 <= beamPos.real < len(data) and 0 <= beamPos.imag < len(data[0])):
             continue
-        match data[nextPos[0]][nextPos[1]]:
-            case  '.': beams.append((nextPos, beamDirection))
-            case  '/': beams.append((nextPos, (-beamDirection[1], -beamDirection[0])))
-            case '\\': beams.append((nextPos, (beamDirection[1], beamDirection[0])))
-            case  '|': beams += [(nextPos, (-1,0)), (nextPos, (1,0))]
-            case  '-': beams += [(nextPos, (0,-1)), (nextPos, (0,1))]
+        match data[int(beamPos.real)][int(beamPos.imag)]:
+            case  '.': beams.append((beamPos, beamDirection))
+            case  '/': beams.append((beamPos, -1j/beamDirection))
+            case '\\': beams.append((beamPos, 1j/beamDirection))
+            case  '|': beams += [(beamPos, -1), (beamPos, 1)]
+            case  '-': beams += [(beamPos, -1j), (beamPos, 1j)]
     return len(set(pos for (pos, _) in seen)) - 1
 
-print("Problem 31:", simulateEnergized((0,-1), (0,1)))
+print("Problem 31:", simulateEnergized(-1j, 1j))
 
-starts = [((i,-1),(0,1)) for i in range(len(data))] + [((-1,j),(1,0)) for j in range(len(data[0]))] \
-       + [((i,len(data)),(0,-1)) for i in range(len(data))] + [((len(data[0]),j),(-1,0)) for j in range(len(data[0]))]
+starts = [(complex(i,-1),1j) for i in range(len(data))] + [(complex(-1,j),1) for j in range(len(data[0]))] \
+       + [(complex(i,len(data)),-1j) for i in range(len(data))] + [(complex(len(data[0]),j),-1) for j in range(len(data[0]))]
 
 print("Problem 32:", max(simulateEnergized(*info) for info in starts))
