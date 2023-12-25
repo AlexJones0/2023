2fc2afa549f08f144203efb99901cb799cb1ebfc
xeovz <65985521+xeovz@users.noreply.github.com>
2023-12-25 10:49:50 +0000

    Day 25 python solutions

--- Day 25/Python/sol.py
+++ Day 25/Python/sol.py
@@ -4,10 +4,17 @@ Author: Alex Jones
 Desc: Solution to day 25 problems (49 & 50) for Advent of Code 2023, solved in Python 3.
 """
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = open("Day 25/data.txt", "r").read().strip().splitlines()
+data = [line.split(": ") for line in open("Day 25/data.txt", "r").read().strip().splitlines()]
 
+import networkx
+from math import prod
 
-
-print("Problem 49:", NOT_IMPLEMENTED)
-
-print("Problem 50:", NOT_IMPLEMENTED)
+graph = networkx.Graph({wire: other.split() for wire, other in data})
+networkx.set_edge_attributes(graph, {e:{'capacity':1} for e in graph.edges()})
+cutValue, components = 0, tuple()
+for source, sink in [(a,b) for a in graph.nodes() for b in graph.nodes() if a != b]:
+    cutValue, components = networkx.minimum_cut(graph, source, sink)
+    if cutValue == 3:
+        break
+print("Problem 49:", prod(len(component) for component in components))
+print("Problem 50:", "Freebie :)")
